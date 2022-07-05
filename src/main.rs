5869b94068b3498efe4213f004fea17bd399a410
EddyBautista <bautista.j.eddy@gmail.com>
2022-07-05 16:28:29 -0500

    complete guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,35 @@
-//We need to bring out the input / output libary into scope which comes from the standard libary know as std:
-use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
-// main function is the entry point into the program.
-// the fn syntax declares a new function.
 fn main() {
-    println!("Guess the number");
-    println!("Please input your guess");
+    println!("Guess the number!");
 
-    // Secret number
-    let secret_number = rand::thread_rng().gen_range(1..10);
-// variables are immutable by default, adding mut makes it mutable.
-    let mut guess = String::new();
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-    println!("The Secret Number was : {}", secret_number);
+    loop {
+        println!("Please input your guess.");
 
-    println!("You guessed: {}", guess);
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
